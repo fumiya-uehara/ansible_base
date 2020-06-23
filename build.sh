b2b78377fc56ec4cafcf3c902749e66f76a45b22
fumiya-uehara <edenofeast515@outlook.jp>
2020-06-23 12:51:35 +0900

    add root以外でログインできるように設定を追加

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --rm -t local/c7.8-systemd ./base_image
 docker build --rm -t local/c7.8-systemd-sshd ./sshd_image
-docker run -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 12222:22 local/c7.8-systemd-sshd
+docker run -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 12222:22 --name test_ansible local/c7.8-systemd-sshd
